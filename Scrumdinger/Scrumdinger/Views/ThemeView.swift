b86b97430a318dcd08f60fec24f0f00f4d02a5b1
Diego Trazzi <dtrazzi@apple.com>
2022-09-08 21:26:01 -0700

    restuctured

---
+++ Scrumdinger/Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,20 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by mac_sys1 on 9/8/22.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView()
+    }
+}

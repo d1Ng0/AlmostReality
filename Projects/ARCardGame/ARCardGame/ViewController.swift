e6d0873fbc8f2d0506005452f041f70cec112f31
Diego Trazzi <dtrazzi@apple.com>
2022-09-15 16:41:26 -0700

    ARView Demo working with 3D tapGesture

--- Projects/ARCardGame/ARCardGame/ViewController.swift
+++ Projects/ARCardGame/ARCardGame/ViewController.swift
@@ -2,33 +2,27 @@ import UIKit
 import RealityKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet var arView: ARView!
-    
+
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
-        let path = Bundle.main.path(forResource: "assets", ofType: nil)
-        let items = try! FileManager.default.contentsOfDirectory(atPath: path!)
-        for var item in items {
-            item = (item as NSString).deletingPathExtension
-            print(item)
-        }
-        
         // Create an anchor for horizontal plane
-        
-//        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.1, 0.1])
-        let anchor = AnchorEntity()
-        
+        let anchor = AnchorEntity(.plane(.horizontal, classification: .table, minimumBounds: [0.1, 0.1]))
         arView.scene.addAnchor(anchor)
         
-        // Load Model Assets
+        // Load 3D-card Templates
         var cardTemplates: [Entity] = []
-        // Load model assets for cards into an array
-        for assetName in items {
-            let cardTemplate = try! Entity.loadModel(named: assetName)
-            cardTemplates.append(cardTemplate)
+        if let assetURLs = Bundle.main.urls(forResourcesWithExtension: "usdz", subdirectory: "") {
+            for url in assetURLs {
+                var assetName = url.relativeString
+                assetName = (assetName as NSString).deletingPathExtension
+                let cardTemplate = try! Entity.loadModel(named: assetName)
+                cardTemplate.name = assetName
+                cardTemplate.generateCollisionShapes(recursive: true)
+                arView.installGestures(for: cardTemplate)
+                cardTemplates.append(cardTemplate)
+            }
         }
         
         // Create cards
@@ -41,14 +35,24 @@ class ViewController: UIViewController {
         
         // Build the board
         cards.shuffle()
+        
         // Position cards
         for (index, card) in cards.enumerated() {
             let x = Float(index % 4) - 1.5
             let z = Float(index / 4) - 1.5
-            card.position = [x * 0.13, 0, z * 0.13]
+            card.position = [x * 0.07, 0, z * 0.07]
             anchor.addChild(card)
         }
         
         
+        arView.debugOptions = .showPhysics
     }
+        
+        // Hit Testing
+        @IBAction func  onTap (_ sender: UITapGestureRecognizer) {
+            let tapLocation = sender.location(in: arView)
+            if let card = arView.entity(at: tapLocation) {
+                print(card.name)
+            }
+        }
 }
---
+++ Projects/RealityKitSimpleGesture/RealityKitSimpleGesture/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RealityKitSimpleGesture
+//
+//  Created by mac_sys1 on 9/14/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}

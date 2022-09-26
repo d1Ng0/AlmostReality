4e66c3e781cce66bde2f3e30b59e259775d6d236
Diego Trazzi <dtrazzi@apple.com>
2022-09-25 20:44:01 -0700

    expreimenting with ARView

---
+++ Projects/SwiftUiARTemplate/SwiftUiARTemplate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUiARTemplate
+//
+//  Created by mac_sys1 on 9/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/SwiftUiARTemplate/SwiftUiARTemplate/SwiftUiARTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiARTemplateApp.swift
+//  SwiftUiARTemplate
+//
+//  Created by mac_sys1 on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiARTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
